11949e5878daac27b608ef99c559957a57f84466
CristinaaPichiu <cristinapichiu38@gmail.com>
2026-04-30 14:12:51 +0300

    initial project structure

---
+++ src/main/java/com/example/activitylog/CassandraActivityLogAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.activitylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraActivityLogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraActivityLogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/activitylog/CassandraActivityLogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.activitylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandraActivityLogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
